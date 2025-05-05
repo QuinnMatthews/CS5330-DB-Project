522c2851a0339dda59cd56c0d50748be574d2a45
Matthews, Quinn <Quinn.Matthews@lrs.com>
2025-05-04 19:59:23 -0500

    remove unusued import

--- social-media-research-database/app/api/projects/[project_name]/fields/route.ts
+++ social-media-research-database/app/api/projects/[project_name]/fields/route.ts
@@ -1,7 +1,6 @@
 import { NextResponse, NextRequest } from "next/server";
 import { queryDB } from "@/app/api/utils";
 import { z } from "zod";
-import { Project } from "@/app/projects/types";
 
 export const fieldSchema = z.object({
     field_name: z.string().min(1).max(100),
@@ -19,7 +18,7 @@ export async function POST(request: NextRequest, context: { params: { project_na
 
 
     try {
-    const query = `INSERT INTO field (project_name, name) VALUES (?,?)`;
+        const query = `INSERT INTO field (project_name, name) VALUES (?,?)`;
         const results = await queryDB(query, [projectName, parseResult.data.field_name]);
         return NextResponse.json(results);
     } catch (err: any) {
